c7ff150fb9890a0285e1ac3d8042a6f25e6aa06a
WolbachAuth0 <aaron.wolbach@okta.com>
2024-01-23 13:56:37 -0500

    initial commit

---
+++ web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <hr />
+    <home-content />
+  </div>
+</template>
+
+<script lang="ts">
+import HomeContent from "../components/HomeContent.vue";
+
+export default {
+  name: "home-view",
+  components: {
+    HomeContent,
+  },
+};
+</script>
+
