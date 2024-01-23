266d813690dfccebf327af147aece37de82fd71e
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-23 14:55:04 -0600

    Update middleware.ts

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -10,6 +10,8 @@ export default async function middleware(req: NextRequest) {
     try {
         if (token && await verifyJWT(token)) {
             return NextResponse.next();
+        } else {
+            return NextResponse.json({ message: 'Authorization Required'}, { status: 401 })
         }
     } catch (e) {
         return NextResponse.json({ message: e }, { status: 401 })
