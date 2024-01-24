45500f97549fdb8e3558ee9bf71fd6c8f41a9459
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-24 16:21:09 -0600

    CORS BRO

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyJWT } from '@/utils/token_utils';
+import { setCorsMiddlewareHeaders } from './utils/header_utils';
 
 export const config = {
     matcher: '/api/:path*'
@@ -7,24 +8,16 @@ export const config = {
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
     if (req.method === 'OPTIONS') {
-        return setCorsHeaders(res).ok;
+        return setCorsMiddlewareHeaders(res).ok;
     }
 
     const response = NextResponse.next();
 
     const token = req.headers.get('authorization')?.split(' ')[1];
     if (token && await verifyJWT(token)) {
-        return setCorsHeaders(response);
+        return setCorsMiddlewareHeaders(response);
     }
 
     const authFailedResponse = NextResponse.json({ message: 'Authorization Required'}, { status: 401 })
-    return setCorsHeaders(authFailedResponse);
-}
-
-function setCorsHeaders(response: NextResponse) {
-    response.headers.set("Access-Control-Allow-Credentials", "true");
-    response.headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT,OPTIONS");
-    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    return response;
+    return setCorsMiddlewareHeaders(authFailedResponse);
 }
--- api/expense-api/src/pages/api/fga-list-all.ts
+++ api/expense-api/src/pages/api/fga-list-all.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getFGAJWT } from '@/utils/token_utils';
 import { listAllTuples } from '@/utils/fga_utils';
+import { setCorsApiHeaders } from '@/utils/header_utils';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const payload = req.body;
@@ -10,7 +11,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (fga_token) {
         const result = await listAllTuples(fga_token, payload);
 
-        return res.status(200).json({
+        return setCorsApiHeaders(res).status(200).json({
             result: result
         });
     }
