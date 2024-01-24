795047fbfb4c3a029f5c7fa2f083884deede2853
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-24 17:21:35 -0600

    going crazy

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -1,17 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyJWT } from '@/utils/token_utils';
+import cors from './utils/cors';
 
 export const config = {
     matcher: '/api/:path*'
 }
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
-    const response = NextResponse.next();
+    let response = null;
 
     const token = req.headers.get('authorization')?.split(' ')[1];
     if (token && await verifyJWT(token)) {
-        return response;
+        return NextResponse.next();
+    } else {
+        response = NextResponse.json({ message: 'Authorization Required'}, { status: 401 });
     }
-
-    return NextResponse.json({ message: 'Authorization Required'}, { status: 401 })
+    
+    return cors(req, res);
 }
