af0f21017ca264ff6730782ecf57217ec0a990e2
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-24 16:25:25 -0600

    next cors

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -1,23 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyJWT } from '@/utils/token_utils';
-import { setCorsMiddlewareHeaders } from './utils/header_utils';
 
 export const config = {
     matcher: '/api/:path*'
 }
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
-    if (req.method === 'OPTIONS') {
-        return setCorsMiddlewareHeaders(res).ok;
-    }
-
     const response = NextResponse.next();
 
     const token = req.headers.get('authorization')?.split(' ')[1];
     if (token && await verifyJWT(token)) {
-        return setCorsMiddlewareHeaders(response);
+        return response;
     }
 
-    const authFailedResponse = NextResponse.json({ message: 'Authorization Required'}, { status: 401 })
-    return setCorsMiddlewareHeaders(authFailedResponse);
+    return NextResponse.json({ message: 'Authorization Required'}, { status: 401 })
 }
--- api/expense-api/src/pages/api/fga-list-all.ts
+++ api/expense-api/src/pages/api/fga-list-all.ts
@@ -1,9 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getFGAJWT } from '@/utils/token_utils';
 import { listAllTuples } from '@/utils/fga_utils';
-import { setCorsApiHeaders } from '@/utils/header_utils';
+import NextCors from 'nextjs-cors';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
     const payload = req.body;
 
     const fga_token = await getFGAJWT();
@@ -11,7 +18,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (fga_token) {
         const result = await listAllTuples(fga_token, payload);
 
-        return setCorsApiHeaders(res).status(200).json({
+        return res.status(200).json({
             result: result
         });
     }
