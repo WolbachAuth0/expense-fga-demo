e6a8d5466aebe0571da0a229bc4dfaf0cd5e075f
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-24 15:48:19 -0600

    CORS debugging

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -6,6 +6,14 @@ export const config = {
 }
 
 export default async function middleware(req: NextRequest) {
+    const response = NextResponse.next();
+    
+    // Set default CORS headers
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+    response.headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT,OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+    response.headers.set("Access-Control-Allow-Origin", "*");
+
     const token = req.headers.get('authorization')?.split(' ')[1];
     if (token && await verifyJWT(token)) {
         return NextResponse.next();
