f70ff1ba31dd7d64283ffceade801b974911d6a5
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-02 00:14:21 -0500

    add placeholder content

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -30,7 +30,7 @@
           <v-card-body>
             <p>Explain how the Authorization model works.</p>
             <p>Explain which users to login with.</p>
-            <p v-for="i in 11" :key="i">blah blah blahbity blah blah</p>
+            <p v-for="i in 9" :key="i">blah blah blahbity blah blah</p>
           </v-card-body>
         </v-card>
       </v-col>
@@ -39,6 +39,28 @@
         <v-card class="mr-2 p-2">
           <v-card-subtitle class="bg-primary text-white">User Organization</v-card-subtitle>
           <v-img class="align-end" :src="orgChart.dark" cover></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="5">
+        <v-card class="ml-2 p-2">
+          <v-card-subtitle class="bg-primary text-white">How To Use This App</v-card-subtitle>
+          <v-card-body>
+            <p>Explain how the Authorization model works.</p>
+            <p>Explain which users to login with.</p>
+            <p v-for="i in 9" :key="i">blah blah blahbity blah blah</p>
+          </v-card-body>
+        </v-card>
+      </v-col>
+
+      <v-col cols="7">
+        <v-card class="mr-2 p-2">
+          <v-card-subtitle class="bg-primary text-white">Authorization Model</v-card-subtitle>
+          <div class="row">
+            <highlightjs language="yaml" :code="authModel" />
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -60,7 +82,19 @@ export default {
       architecture: {
         light: '/img/architecture-light.png',
         dark: '/img/architecture-dark.png'
-      }
+      },
+      authModel: `model
+  schema 1.1
+
+type employee
+  relations
+    define can_manage: manager or can_manage from manager
+    define manager: [employee]
+
+type report
+  relations
+    define approver: can_manage from submitter
+    define submitter: [employee]`
     }
   }
 };
