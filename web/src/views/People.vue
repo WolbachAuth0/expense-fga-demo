51e8fefdb566cf84596cd6cd8bb9f1a7938004ea
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-06 23:28:50 -0500

    people page placeholders

--- web/src/views/People.vue
+++ web/src/views/People.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <v-card title="People">
-        
-      </v-card>
-    </v-col>
-  </v-row>
+   <v-card class="bg-secondary">
+    <v-card-title class="bg-primary text-white">People</v-card-title>
+
+    <v-row>
+      <v-col cols="12">
+        <v-card class="ma-2 pa-2">
+          <v-card-subtitle class="bg-primary text-white">User Organization</v-card-subtitle>
+          <v-img class="align-end" :src="orgChart.dark" cover></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </v-card>
 </template>
 
 <script>
@@ -14,6 +20,10 @@ export default {
   name: "People",
   data () {
     return {
+      orgChart: {
+        light: '/img/org-chart-light.png',
+        dark: '/img/org-chart-dark.png',
+      }
     }
   }
 };
