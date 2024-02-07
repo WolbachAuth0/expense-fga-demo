2bde08812f30e0d534c9ab86a7da09281d7e0ed9
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-06 23:28:38 -0500

    padding and margins

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -5,18 +5,18 @@
 
     <v-row class="py-1">
       <v-col cols="5">
-        <v-card class="ml-2">
+        <v-card class="ml-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">Application Architecture</v-card-subtitle>
-          <v-card-body>
+          <v-card-text>
             <p>Explain in this section how the app is architected.</p>
             <p>Describe how the authN and authZ flow works.</p>
             <p v-for="i in 22" :key="i">blah blah blahbity blah blah</p>
-          </v-card-body>
+          </v-card-text>
         </v-card>
       </v-col>
 
       <v-col cols="7">
-        <v-card class="mr-2 p-2">
+        <v-card class="mr-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">Authorization Flow</v-card-subtitle>
           <v-img class="align-end" :src="architecture.dark" cover></v-img>
         </v-card>
@@ -25,18 +25,18 @@
 
     <v-row>
       <v-col cols="5">
-        <v-card class="ml-2 p-2">
+        <v-card class="ml-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">How To Use This App</v-card-subtitle>
-          <v-card-body>
+          <v-card-text>
             <p>Explain how the Authorization model works.</p>
             <p>Explain which users to login with.</p>
             <p v-for="i in 9" :key="i">blah blah blahbity blah blah</p>
-          </v-card-body>
+          </v-card-text>
         </v-card>
       </v-col>
 
       <v-col cols="7">
-        <v-card class="mr-2 p-2">
+        <v-card class="mr-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">User Organization</v-card-subtitle>
           <v-img class="align-end" :src="orgChart.dark" cover></v-img>
         </v-card>
@@ -45,18 +45,18 @@
 
     <v-row>
       <v-col cols="5">
-        <v-card class="ml-2 p-2">
+        <v-card class="ml-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">How To Use This App</v-card-subtitle>
-          <v-card-body>
+          <v-card-text>
             <p>Explain how the Authorization model works.</p>
             <p>Explain which users to login with.</p>
             <p v-for="i in 9" :key="i">blah blah blahbity blah blah</p>
-          </v-card-body>
+          </v-card-text>
         </v-card>
       </v-col>
 
       <v-col cols="7">
-        <v-card class="mr-2 p-2">
+        <v-card class="mr-2 pa-2">
           <v-card-subtitle class="bg-primary text-white">Authorization Model</v-card-subtitle>
           <div class="row">
             <highlightjs language="yaml" :code="authModel" />
