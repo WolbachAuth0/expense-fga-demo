137d1effef8361fb3786cf102316fb6f77a65010
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-12 13:59:49 -0500

    refactor api service

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -21,20 +21,25 @@ function user (auth) {
   return auth.user._value
 } 
 
-export async function getReports (auth) {
-  const url = '/list-reports'
-  const data = {
-    user_id: user(auth).sub
-  }
-
+async function post (auth, url, data) {
   try {
     const accesstoken = await auth.getAccessTokenSilently()
     const response = await http(accesstoken).post(url, data)
     return response.data
   } catch (error) {
-    console.log(error)
+    if (error.response.status == 401) {
+      return error.response.data
+    }
     return error
   }
+}
+
+export async function getReports (auth) {
+  const url = '/list-reports'
+  const data = {
+    user_id: user(auth).sub
+  }
+  return await post(auth, url, data)
 }
 
 export async function submitReport (auth, { amount, merchant, description }) {
@@ -46,14 +51,7 @@ export async function submitReport (auth, { amount, merchant, description }) {
     submitter_id: user(auth).sub,
     submitter_email: user(auth).email
   }
-  
-  try {
-    const accesstoken = await auth.getAccessTokenSilently()
-    const response = await http(accesstoken).post(url, data)
-    return response.data
-  } catch (error) {
-    return error
-  }
+  return await post(auth, url, data)
 }
 
 export async function approveReport (auth, report_id) {
@@ -85,15 +83,16 @@ export async function disapproveReport (auth, report_id) {
     approver_id: user(auth).sub,
     approver_email: user(auth).email
   }
+  return await post(auth, url, data)
+  
+}
 
-  try {
-    const accesstoken = await auth.getAccessTokenSilently()
-    const response = await http(accesstoken).post(url, data)
-    return response.data
-  } catch (error) {
-    if (error.response.status == 401) {
-      return error.response.data
-    }
-    return error
+export async function deleteReport (auth, report_id) {
+  const url = '/delete-report'
+  const data = {
+    report_id,
+    approver_id: user(auth).sub,
+    approver_email: user(auth).email
   }
+  return await post(auth, url, data)
 }
