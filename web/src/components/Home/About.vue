4b4a15b518b4c949878277d371d0814916b2fc9c
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-12 09:58:42 -0500

    add step by step description of auth flow

--- web/src/components/Home/About.vue
+++ web/src/components/Home/About.vue
@@ -17,8 +17,46 @@
               an authenticated user to interact with the UI, the SPA communicates with the REST API and the
               REST API brokers changes to the database on behalf of the user.
             </p>
-          </v-card-text>
+            <br/>
+            <p>
 
+            </p>
+          </v-card-text>
+          
+          <v-card-subtitle class="bg-primary text-white">Authentication</v-card-subtitle>
+          
+          <v-list lines="one">
+            <v-list-subheader title="User Authentication"></v-list-subheader>
+
+            <v-list-item 
+              v-for="item in authNItems"
+              :key="item.value"
+              :title="item.title"
+            >
+              <v-tooltip activator="parent" location="end" v-if="item.tooltip">
+                {{ item.tooltip }}
+              </v-tooltip>
+            </v-list-item>
+
+            <v-list-subheader title="Authorization"></v-list-subheader>
+
+            <v-list-item 
+              v-for="item in authZItems"
+              :key="item.value"
+              :title="item.title"
+            >
+              <v-tooltip activator="parent" location="end" v-if="item.tooltip">
+                {{ item.tooltip }}
+              </v-tooltip>
+            </v-list-item>
+          </v-list>
+
+          <v-card-text>
+            <p>
+              
+            </p>
+          </v-card-text>
+          
         </v-card>
       </v-col>
 
@@ -47,6 +85,45 @@ export default {
         light: '/img/architecture-light.png',
         dark: '/img/architecture-dark.png'
       },
+      authNItems: [
+        {
+          value: 1,
+          title: '1. User navigates to application',
+          tooltip: ''
+        },
+        {
+          value: 2,
+          title: '2. User clicks login',
+          tooltip: 'User is redirected to Auth0 Universal Login (UL)'
+        },
+        {
+          value: 3,
+          title: '3. User submits credentials to Auth0',
+          tooltip: 'Credentials are sent to the Auth0 server for verification'
+        },
+        {
+          value: 4,
+          title: '4. Succeful Authentication',
+          tooltip: ' Auth0 redirects user back to application with tokens'
+        },
+      ],
+      authZItems: [
+        {
+          value: 5,
+          title: '5. User interaction',
+          tooltip: `The front end sends http requests to the REST API.`
+        },
+        {
+          value: 6,
+          title: '6. Middleware calls Auth0 FGA API',
+          tooltip: 'Check that user is authorized to perform action'
+        },
+        {
+          value: 7,
+          title: '7. CRUD action to database',
+          tooltip: 'If the the user is authorized, persist data and respond to request.'
+        },
+      ]
     }
   }
 }
