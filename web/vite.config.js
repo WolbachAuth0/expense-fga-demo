cd6acc0b62cb0459fc98c74a9b9156e5014802e5
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-12 11:35:52 -0500

    update vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#multi-page-app
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [ vue() ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reports: resolve(__dirname, 'reports/index.html'),
+        profile: resolve(__dirname, 'profile/index.html'),
+      },
+    },
+  }
 })
