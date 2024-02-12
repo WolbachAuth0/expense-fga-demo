329a476860082353eb1d18a72165de9b9a28b5e3
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-12 15:51:00 -0600

    fixing bad middleware logic

--- api/expense-api/src/middleware.ts
+++ api/expense-api/src/middleware.ts
@@ -16,9 +16,9 @@ export default async function middleware(req: NextRequest, res: NextResponse) {
             res.headers.set('extracted_requester_id', decoded_token.sub);
             res.headers.set('extracted_requester_email', decoded_token['email'] as string);
         }
-    } 
-        
-    res = NextResponse.json({ message: 'Authorization Required'}, { status: 401 });
-
+    } else {
+        res = NextResponse.json({ message: 'Authorization Required'}, { status: 401 });
+    }    
+    
     return cors(req, res);
 }
