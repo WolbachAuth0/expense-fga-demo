dca3a277f723f4b6b0bd0381c4f629d2ceb46ed2
WolbachAuth0 <aaron.wolbach@okta.com>
2024-02-15 09:51:12 -0500

    fix github position

--- web/src/components/GithubRibbon.vue
+++ web/src/components/GithubRibbon.vue
@@ -4,7 +4,7 @@
 			width="80"
 			height="80"
 			viewBox="0 0 250 250"
-			style="fill:#1976D2; color:#fff; position: absolute; top: 65; border: 0; right: 0; z-index: 1000;"
+			style="fill:#1976D2; color:#fff; position: absolute; top: 64; border: 0; right: 0; z-index: 1000;"
 			aria-hidden="true"
 		>
 			<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
