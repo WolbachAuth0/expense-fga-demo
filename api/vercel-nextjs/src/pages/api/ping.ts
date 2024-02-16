ee4176a13beeaba7219e860d135e9e62e75b1aa9
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-16 13:56:31 -0600

    Update ping.ts

--- api/vercel-nextjs/src/pages/api/ping.ts
+++ api/vercel-nextjs/src/pages/api/ping.ts
@@ -1,9 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Endpoint for API status checks
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json({ message: 'up' });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "up" });
 }
