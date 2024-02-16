9f519cf71667879810b6a56af947d8fa140d2069
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-16 17:11:52 -0600

    Update auth.middleware.ts

--- api/render-nestjs/src/auth/auth.middleware.ts
+++ api/render-nestjs/src/auth/auth.middleware.ts
@@ -6,7 +6,7 @@ import { UnauthorizedError } from 'express-oauth2-jwt-bearer';
 export class AuthMiddleware implements NestMiddleware {
   async use(req: any, res: any, next: (error?: any) => void) {
     console.log('****this is class middleware*****');
-    const token = req.headers.authorization.split('Bearer ')[1];
+    const token = req.headers?.authorization?.split('Bearer ')[1];
     console.log(token);
     if (!token) {
       next(new UnauthorizedError());
