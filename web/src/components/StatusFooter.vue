9f83309fd7328a413b4b4c97245fce87063941c8
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-26 17:09:01 -0600

    skinny the font for api status

--- web/src/components/StatusFooter.vue
+++ web/src/components/StatusFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <v-footer class="bg-grey-darken-4" style="height: 50px; position:fixed; bottom:0; width: 100%">
         <v-row justify="center" no-gutters>
-            <b>API Status: <v-icon class="ml-2" size="x-small" :color="statusColor" icon="mdi-circle"></v-icon> </b>
+            <span>API Status: <v-icon class="ml-2" size="x-small" :color="statusColor" icon="mdi-circle"></v-icon> </span>
         </v-row>
     </v-footer>
 </template>
