b2dc6929c2caf391a0fcd6ba06ee97d0cbc4fa08
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-26 18:32:30 -0600

    Update fetch-jwks.ts

--- api/vercel-nextjs/src/pages/api/fetch-jwks.ts
+++ api/vercel-nextjs/src/pages/api/fetch-jwks.ts
@@ -8,5 +8,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const result = await fetch(
     `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
   );
-  res.status(200).json({ message: "done", result: result });
+  const data = await result.json();
+  res
+    .status(200)
+    .json({
+      message: "done",
+      result: data,
+      url: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+    });
 };
