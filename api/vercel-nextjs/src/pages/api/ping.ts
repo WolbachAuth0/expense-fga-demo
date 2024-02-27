50ae04ea1d427e488de6ec40663789165f920917
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-26 18:17:16 -0600

    nvm

--- api/vercel-nextjs/src/pages/api/ping.ts
+++ api/vercel-nextjs/src/pages/api/ping.ts
@@ -1,10 +1,6 @@
 // Endpoint for API status check
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ message: "up" });
 }
