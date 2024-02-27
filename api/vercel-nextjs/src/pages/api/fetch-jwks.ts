59da7715310d2555dfc0b43be1fd305e9e76db3a
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-02-26 18:19:20 -0600

    troubleshooting

--- api/vercel-nextjs/src/pages/api/fetch-jwks.ts
+++ api/vercel-nextjs/src/pages/api/fetch-jwks.ts
@@ -3,6 +3,6 @@ import { fetchAndCacheJWKS } from "@/utils/token_utils";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await fetchAndCacheJWKS();
-  res.status(200).json({ message: "done" });
+  const result = await fetchAndCacheJWKS();
+  res.status(200).json({ message: "done", result: result });
 };
